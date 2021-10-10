be30b48d76a454b152e8f8404d763ab807c93424
vmikhalskyi <vladmikh31@gmail.com>
2021-10-10 20:56:27 +0300

    Lab1

---
+++ lab1/1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace _1._2
+{
+    class Program
+    {
+        public static byte[] GenerateRandomNumber(int length) // новый метод public 
+        {
+            using (var rndNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                rndNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 10; i++)
+            {
+                string randomNumber = Convert.ToBase64String(GenerateRandomNumber(32));
+                Console.WriteLine(randomNumber);
+            }
+
+        }
+    }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(1234);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(random.Next(4, 20));
+            }
+
+        }
+    }
+}
